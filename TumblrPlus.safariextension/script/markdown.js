1b6a73c41d242bac02f3fc359720fff13aacf812
Dov Frankel <dov@abbey-code.com>
2012-09-24 18:46:22 -0400

    Fixed bug caused when markdown reference-style links are repeated

--- TumblrPlus.safariextension/script/markdown.js
+++ TumblrPlus.safariextension/script/markdown.js
@@ -1,34 +1,52 @@
 function defineLinks(post) {
+    // Get the list of undefined links
+    var undefinedLinks = findUndefinedLinks(post);
+    
+    // Add link definitions to the end of the post
+    var result = post;
+    
+    // Add a gap before the new links
+    if (undefinedLinks.length > 0) {
+        result += '\n\n';
+    }
+    
+    // Add a definition for each link at the bottom
+    for (i = 0; i < undefinedLinks.length; i++) {
+        var link = undefinedLinks[i];
+        result += '\n' + link + ':';
+    }
+    
+    // Return the enhanced post
+    return result;
+}
+
+function findUndefinedLinks(post) {
+    // Define regexes used
     var usedLinkPattern = /\[[^\]]+\]\s*(\[[^\]]+\])/gm;
     var usedLinks = new Array();
     
-    while ((match = usedLinkPattern.exec(post)) && usedLinks.indexOf(match[1]) == -1) {
-        usedLinks.push(match[1]);
+    // Find all link names used in the post
+    while ((match = usedLinkPattern.exec(post))) {
+        // Add links not already discovered to the list
+        if (usedLinks.indexOf(match[1]) == -1) {
+            usedLinks.push(match[1]);
+        }
     }
     
     var undefinedLinks = new Array();
     
+    // Look to see if each one is defined somewhere else
     for (i = 0; i < usedLinks.length; i++) {
         var usedLink = usedLinks[i];
         var regExLink = usedLink.replace('[', '\\[').replace(']', '\\]');
         
         var definedLinkPattern = new RegExp(regExLink + '\\s*:', 'gm');
         
+        // The link is not defined, save it
         if (!definedLinkPattern.test(post)) {
             undefinedLinks.push(usedLink);
         }
     }
     
-    var result = post;
-    
-    if (undefinedLinks.length > 0) {
-        result += '\n\n';
-    }
-    
-    for (i = 0; i < undefinedLinks.length; i++) {
-        var link = undefinedLinks[i];
-        result += '\n' + link + ':';
-    }
-    
-    return result;
+    return undefinedLinks;
 }
--- tests/markdown-tests.js
+++ tests/markdown-tests.js
@@ -4,9 +4,10 @@ load('common.js');
 load('../TumblrPlus.safariextension/script/markdown.js');
 
 // Run all tests
-testLinkCollection();
+testDefineLinks();
+testFindUndefinedLinks();
 
-function testLinkCollection() {
+function testDefineLinks() {
     var input = new Array();
     var expected = new Array();
     
@@ -59,7 +60,7 @@ function testLinkCollection() {
                   '\n' +
                   '\n' +
                   '[link3]:');
-    
+
     assert(input.length == expected.length, "Input and expected output counts don't match");
     
     for (var i = 0; i < input.length; i++) {
@@ -68,5 +69,72 @@ function testLinkCollection() {
         assert(expecting == output, '\n  Expected:\n' + expecting + '\n  Got:\n' + output);
     }
     
-    print('testLinkCollection passed');
+    print('testDefineLinks passed');
+}
+
+function testFindUndefinedLinks() {
+    var input = new Array();
+    var expected = new Array();
+    
+    input.push   ('This is a [link1][link2]');
+    expected.push(['[link2]']);
+    
+    input.push   ('These are [two][link] links to the [same][link] thing');
+    expected.push(['[link]']);
+    
+    input.push   ('No links here');
+    expected.push([]);
+    
+    input.push   ('Line contains a backslash `C:\\dos.bat`');
+    expected.push([]);
+    
+    input.push   ('Line contains a backslash `C:\\dos.bat` [and a link][link-name]');
+    expected.push(['[link-name]']);
+    
+    input.push   ('This is a [link1] [link2]');
+    expected.push(['[link2]']);
+    
+    input.push   ('This is a defined [link1][link2]\n' +
+                  '[link2]:');
+    expected.push([]);
+    
+    input.push   ('This is a defined [link1][link2]\n' +
+                  '[link2] :');
+    expected.push([]);
+    
+    input.push   ('This is a defined [link1][link2] and some more with a [link][link3]\n' +
+                  '\n' +
+                  '[link2]:');
+    expected.push(['[link3]']);
+    
+    // Bug reproduced, then fixed
+    input.push   ('This is a defined [link1][link2] and a different [link][link3]\n' +
+                  '\n' +
+                  'Followed by the first [link4][link2] repeated, with a different [link][link5] after' +
+                  '\n' +
+                  '[link2]:');
+    expected.push(['[link3]',
+                   '[link5]']);
+
+    for (var i = 0; i < input.length; i++) {
+        var expecting = expected[i];
+        var output = findUndefinedLinks(input[i]);
+        
+        var passed = true;
+
+        if (expecting.length != output.length) {
+            passed = false;
+        } else {
+            for (var x = 0; x < expecting.length; x++) {
+                if (expecting[x] != output[x]) {
+                    passed = false;
+                    break;
+                }
+            }
+        }
+
+        assert(passed, '\n  Expected:\n' + expecting + '\n  Got:\n' + output);
+    }
+    
+    print('testFindUndefinedLinks passed');
 }
